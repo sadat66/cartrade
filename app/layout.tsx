e4366a77193c4bddb96c1c3728997645a13206fd
zubaerSumon <zubaer.corexlab@gmail.com>
2026-02-24 11:00:53 +0600

    refactor: Move Leaflet CSS import from globals.css to layout.tsx.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import { getLocale } from "next-intl/server";
 import "./globals.css";
+import "leaflet/dist/leaflet.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
